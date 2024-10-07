9d147458b9c8e7945707122599a01507fbc27ea0
spmno <sunqingpeng@hotmail.com>
2024-10-07 14:26:40 +0800

    init commit

---
+++ src/main.rs
@@ -0,0 +1,10 @@
+use async_trait::async_trait;
+use pingora::prelude::*;
+use std::sync::Arc;
+
+fn main() {
+    let mut my_server = Server::new(None).unwrap();
+    my_server.bootstrap();
+    my_server.run_forever();
+}
+
